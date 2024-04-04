742a9f500b5aa3b906c08be5fb587116d6b3c1b2
Matt Wicks <matt.wicks@gmail.com>
2024-04-04 13:55:04 +1100

    Add CompanyEditComponent to app.module.ts and update routing and templates for adding and editing companies

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CompanyListComponent } from './company/company-list/company-list.component';
+import { CompanyEditComponent } from './company/company-edit/company-edit.component';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'company/list' },
@@ -8,6 +9,8 @@ const routes: Routes = [
     path: 'company/list',
     component: CompanyListComponent,
   },
+  { path: 'company/add', component: CompanyEditComponent },
+  { path: 'company/edit/:id', component: CompanyEditComponent },
 ];
 
 @NgModule({

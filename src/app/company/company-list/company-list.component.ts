54b7ddfcd5555707bfc40ea85885c657ce4fe32e
Matt Wicks <matt.wicks@gmail.com>
2024-04-04 11:21:38 +1100

    added company list - stubbed out companies

---
+++ src/app/company/company-list/company-list.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { Company } from '../company';
+
+@Component({
+  selector: 'fbc-company-list',
+  templateUrl: './company-list.component.html',
+  styleUrl: './company-list.component.scss'
+})
+export class CompanyListComponent implements OnInit {
+
+  companies: Company[] = [];
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+    this.getCompanies();
+  }
+
+  private getCompanies(): void {
+    this.companies = [
+      { name: 'company 1', email: 'email 1', phone: '111' },
+      { name: 'company 2', email: 'email 2', phone: '111' }
+    ];
+  }
+}
---
+++ src/app/company/company.ts
@@ -0,0 +1,5 @@
+export interface Company {
+  name: string;
+  phone: string;
+  email: string;
+}

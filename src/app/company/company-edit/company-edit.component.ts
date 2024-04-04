c7b6831980a888487e2bd366f9e3d6dce283e003
Matt Wicks <matt.wicks@gmail.com>
2024-04-04 14:19:15 +1100

    Update form in CompanyEditComponent to include validation and submit button

--- src/app/company/company-edit/company-edit.component.ts
+++ src/app/company/company-edit/company-edit.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'fbc-company-edit',
@@ -7,4 +8,25 @@ import { Component } from '@angular/core';
 })
 export class CompanyEditComponent {
 
+  // formGroup = new FormGroup({
+  //   name: new FormControl(''),
+  //   phone: new FormControl(''),
+  //   email: new FormControl(''),
+  // });
+
+  // formGroup = this.fb.group({
+  //   name: this.fb.control(''),
+  //   phone: this.fb.control(''),
+  //   email: this.fb.control(''),
+  // });
+
+  formGroup = this.fb.group({
+    name: ['', [Validators.required, Validators.minLength(3)]],
+    email: ['', [Validators.required, Validators.email]],
+    phone: [''],
+  });
+
+  constructor(
+    private fb: FormBuilder,
+  ) { }
 }
